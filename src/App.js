d548fbcd201f6f3759c20b97277396cf169b6e2a
Dev Test <devtest@Darryls-MacBook-Pro.local>
2019-08-05 16:05:56 +0100

    Current test progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import request from 'superagent';
+import './App.scss';
+import PostList from './components/PostList';
+import "bootstrap/dist/css/bootstrap.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+      error: null,
+    }
+  }
+
+  componentDidMount() {
+    request.get('https://www.reddit.com/hot.json')
+      .end((error, response) => {
+        if (error) {
+          this.setState({
+            error: true
+          });
+        }
+
+        const { body = {} } = response;
+        const { data = {} } = body;
+        const { children = [] } = data;
+
+        this.setState({
+          isLoaded: true,
+          items: children,
+        })
+      });
+  }
+
+  render() {
+
+    var { isLoaded, items } = this.state;
+
+    console.log(items);
+
+    if (!isLoaded) {
+      return (
+        <div>This has not loaded.</div>
+      );
+    }
+    else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <div className="top-bar">Amazing Logo Goes Here</div>
+              <ul>
+                {items.map(item => (
+                  <li key={item.kind}>
+                    <strong>Author:</strong> {item.data.author} | <strong>Link:</strong>   {item.data.permalink}
+                  </li>
+                ))}
+              </ul>
+            <PostList />
+          </header>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
---
+++ src/components/PostList.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../App.scss';
+
+class PostList extends Component {
+    render() { 
+        return ( 
+            <div>This is a component. Ignore Me.</div>
+         );
+    }
+}
+ 
+export default PostList;
